701205ceb757dbfb9be5cfa4549e62bfe4857bfd
y5RjpEqY <gfxv5e@gmail.com>
2017-09-10 06:09:04 +0900

    いらないものの削除と結果のページ

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,38 @@
 class StaticPagesController < ApplicationController
-  def home
+  def index
   end
 
-  def uec
+  def create
+    if params[:fileupload].nil?
+      return redirect_to '/', danger: '画像を選択してください'
+    end
+    uploaded_file = params[:fileupload][:file]
+    filename = uploaded_file.original_filename
+    output_path = Rails.root.join('public/img', filename)
 
+    File.open(output_path, 'w+b') do |fp|
+      fp.write  uploaded_file.read
+    end
+    #   TODO:: ここでshell叩こう
+    #   返り値によってrenderのページを変更する
+    cmd = 'pwd'
+    res = `#{cmd}`
+    case res
+      when 0 then
+        redirect_to '/0'
+      when 1 then
+        # redirect_to :action => 'index'
+      when 2 then
+        # redirect_to :action => 'index'
+      else
+        # redirect_to '/0'
+    end
   end
 
-  def meiji
-
+  def show
+    @id = params[:id]
   end
 
-  def aogaku
 
-  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
-  get 'uec', to: "static_pages#uec"
-  get 'meiji', to: "static_pages#meiji"
-  get 'aogaku', to: "static_pages#aogaku"
-  resources :image_files
+  resources :static_pages, :only => [:index, :create, :show], path: '/'
 end
